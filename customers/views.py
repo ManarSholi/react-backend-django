0bb80b191a514ab0539e12a82554f49e22102257
ManarSholi <manar.ghassan.sholi@gmail.com>
2024-05-07 10:04:40 +0300

    Edit views

--- customers/views.py
+++ customers/views.py
@@ -1,19 +1,34 @@
 from django.http import JsonResponse, Http404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 from customers.serializers import CustomerSelializer
 from customers.models import Customer
 
 
+@api_view(['GET', 'POST'])
 def customers(request):
     #invoke serializer and return to client
     data = Customer.objects.all()
     serializer = CustomerSelializer(data, many=True)
-    return JsonResponse({'customers': serializer.data})
+    return Response(data={'customers': serializer.data})
 
+@api_view(['GET', 'POST', 'DELETE'])
 def customer(request, id):
     try:
         data = Customer.objects.get(pk=id)
     except Customer.DoesNotExist:
-        raise Http404('Customer does not exist')
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
-    serializer = CustomerSelializer(data)
-    return JsonResponse({'customer': serializer.data})
+    if request.method == 'GET':
+        serializer = CustomerSelializer(data)
+        return Response(data={'customer': serializer.data}, status=status.HTTP_200_OK)
+    elif request.method == 'DELETE':
+        data.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    elif request.method == 'POST':
+        serializer = CustomerSelializer(data, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data={'customer': serializer.data})
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
